f980437937e4dfa0e18edbb9df6122f596f5b5fb
Yonn1981 <105945713+Yonn1981@users.noreply.github.com>
2023-02-16 20:26:57 +0300

    5.5.9zzzzb

--- repo/plugin.video.matrix/resources/sites/aflaam.py
+++ repo/plugin.video.matrix/resources/sites/aflaam.py
@@ -131,7 +131,7 @@ def showMovies(sSearch = ''):
     
 
  # ([^<]+) .+?
-    sPattern = '<span class="label quality">([^<]+)</span>.+? xlink:href="([^<]+)" />.+?data-src="([^<]+)" class="img-fluid w-100 lazy" alt="(.+?)".+?<span class="label rating"><i class="icon-star mr-2">([^<]+)</span>'
+    sPattern = '<span class="label quality">([^<]+)</span>.+?<image x="0" y="0" filter="url(#blur-effect-1)" xlink:href="([^<]+)" />.+?data-src="([^<]+)" class="img-fluid w-100 lazy" alt="(.+?)".+?<span class="label rating"><i class="icon-star mr-2">([^<]+)</span>'
 
     oParser = cParser()
     aResult = oParser.parse(sHtmlContent, sPattern)
@@ -187,7 +187,7 @@ def showSeriesSearch(sSearch = ''):
     oRequestHandler.addHeaderEntry('Accept-Language', 'fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3')
     sHtmlContent = oRequestHandler.request()
  # ([^<]+) .+? (.+?)
-    sPattern = '<span class="label quality">([^<]+)</span>.+? xlink:href="([^<]+)" />.+?data-src="([^<]+)" class="img-fluid w-100 lazy" alt="(.+?)".+?<span class="label rating"><i class="icon-star mr-2">([^<]+)</span>'
+    sPattern = '<span class="label quality">([^<]+)</span>.+?<image x="0" y="0" filter="url(#blur-effect-1)" xlink:href="([^<]+)" />.+?data-src="([^<]+)" class="img-fluid w-100 lazy" alt="(.+?)".+?<span class="label rating"><i class="icon-star mr-2">([^<]+)</span>'
 
     oParser = cParser()
     aResult = oParser.parse(sHtmlContent, sPattern)
@@ -243,7 +243,7 @@ def showSeries(sSearch = ''):
     oRequestHandler.addHeaderEntry('Accept-Language', 'fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3')
     sHtmlContent = oRequestHandler.request()
  # ([^<]+) .+? (.+?)
-    sPattern = '<span class="label quality">([^<]+)</span>.+? xlink:href="([^<]+)" />.+?data-src="([^<]+)" class="img-fluid w-100 lazy" alt="(.+?)".+?<span class="label rating"><i class="icon-star mr-2">([^<]+)</span>'
+    sPattern = '<span class="label quality">([^<]+)</span>.+?<image x="0" y="0" filter="url(#blur-effect-1)" xlink:href="([^<]+)" />.+?data-src="([^<]+)" class="img-fluid w-100 lazy" alt="(.+?)".+?<span class="label rating"><i class="icon-star mr-2">([^<]+)</span>'
 
     oParser = cParser()
     aResult = oParser.parse(sHtmlContent, sPattern)
