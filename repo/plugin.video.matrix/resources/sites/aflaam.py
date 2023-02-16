fee540c3aed92d38d8964f026f338f110d0376ac
Yonn1981 <105945713+Yonn1981@users.noreply.github.com>
2023-02-16 20:15:43 +0300

    5.5.9zzzza

--- repo/plugin.video.matrix/resources/sites/aflaam.py
+++ repo/plugin.video.matrix/resources/sites/aflaam.py
@@ -131,7 +131,7 @@ def showMovies(sSearch = ''):
     
 
  # ([^<]+) .+?
-    sPattern = '<span class="label quality">([^<]+)</span>.+?xlink:href="([^<]+)" class="box">.+?data-src="([^<]+)" class="img-fluid w-100 lazy" alt="(.+?)".+?<span class="icon-star mr-2">([^<]+)</span>'
+    sPattern = '<span class="label quality">([^<]+)</span>.+? xlink:href="([^<]+)" />.+?data-src="([^<]+)" class="img-fluid w-100 lazy" alt="(.+?)".+?<span class="label rating"><i class="icon-star mr-2">([^<]+)</span>'
 
     oParser = cParser()
     aResult = oParser.parse(sHtmlContent, sPattern)
@@ -187,7 +187,7 @@ def showSeriesSearch(sSearch = ''):
     oRequestHandler.addHeaderEntry('Accept-Language', 'fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3')
     sHtmlContent = oRequestHandler.request()
  # ([^<]+) .+? (.+?)
-    sPattern = '<span class="label quality">(.+?)</span>.+?xlink:href="(.+?)" class="box">.+?data-src="(.+?)" class="img-fluid w-100 lazy" alt="(.+?)".+?<span class="icon-star mr-2">(.+?)</span>'
+    sPattern = '<span class="label quality">([^<]+)</span>.+? xlink:href="([^<]+)" />.+?data-src="([^<]+)" class="img-fluid w-100 lazy" alt="(.+?)".+?<span class="label rating"><i class="icon-star mr-2">([^<]+)</span>'
 
     oParser = cParser()
     aResult = oParser.parse(sHtmlContent, sPattern)
@@ -243,7 +243,7 @@ def showSeries(sSearch = ''):
     oRequestHandler.addHeaderEntry('Accept-Language', 'fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3')
     sHtmlContent = oRequestHandler.request()
  # ([^<]+) .+? (.+?)
-    sPattern = '<span class="label quality">([^<]+)</span>.+?xlink:href="([^<]+)" class="box">.+?data-src="([^<]+)" class="img-fluid w-100 lazy" alt="(.+?)".+?<span class="icon-star mr-2">([^<]+)</span>'
+    sPattern = '<span class="label quality">([^<]+)</span>.+? xlink:href="([^<]+)" />.+?data-src="([^<]+)" class="img-fluid w-100 lazy" alt="(.+?)".+?<span class="label rating"><i class="icon-star mr-2">([^<]+)</span>'
 
     oParser = cParser()
     aResult = oParser.parse(sHtmlContent, sPattern)
@@ -296,12 +296,12 @@ def showEpisodes():
 
     oParser = cParser()
 
-    sStart = 'id="series-episodes">'
-    sEnd = '<div class="widget-body row flex-wrap">'
+    sStart = 'id="movie-tab-all">'
+    sEnd = '<div class="tags d-flex flex-wrap mb-4">'
     sHtmlContent = oParser.abParse(sHtmlContent, sStart, sEnd)
   
      # (.+?) ([^<]+) .+?
-    sPattern = 'class="text-white">([^<]+)</a>.+?xlink:href="([^<]+)">.+?<img src="([^<]+)" class="img-fluid" alt='
+    sPattern = 'class="text-white">([^<]+)</a>.+?<a href="([^<]+)">.+?<img src="([^<]+)" class="img-fluid" alt='
     aResult = oParser.parse(sHtmlContent, sPattern)
 	
     if aResult[0] is True:
